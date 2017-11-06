34cc08154227629bdbdae47d1c601866ff33d13b
Jon Jagger <jon@jaggersoft.com>
2017-11-06 08:41:28 +0000

    added test for sub-microsecond datetime-stamp granularity

--- server/src/timeout_cyber_dojo.sh
+++ server/src/timeout_cyber_dojo.sh
@@ -17,7 +17,7 @@ grep -q -c Alpine /etc/issue >/dev/null 2>&1
 if [ $? -eq 0 ]; then
   # On Alpine's ps, the avatar-user's name is truncated to 8 chars
   PS_AVATAR=`echo ${AVATAR} | cut -c -8`
-  su ${AVATAR} -p -c "timeout -s KILL -t ${MAX_SECONDS} sh ./cyber-dojo.sh"
+  su ${AVATAR} -p -c "timeout -s KILL ${MAX_SECONDS}s sh ./cyber-dojo.sh"
   status=$?
 fi
 
