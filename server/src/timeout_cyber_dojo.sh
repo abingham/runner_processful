561f4fc8b1cc4aac5764078df46a3159896d3204
Jon Jagger <jon@jaggersoft.com>
2017-12-01 11:39:30 +0000

    refactoring; improved identifier name

--- server/src/timeout_cyber_dojo.sh
+++ server/src/timeout_cyber_dojo.sh
@@ -3,29 +3,29 @@
 
 readonly IMAGE_NAME=$1
 readonly KATA_ID=$2
-readonly AVATAR=$3
+readonly AVATAR_NAME=$3
 readonly MAX_SECONDS=$4
 
 export CYBER_DOJO_IMAGE_NAME=${IMAGE_NAME}
 export CYBER_DOJO_KATA_ID=${KATA_ID}
-export CYBER_DOJO_AVATAR_NAME=${AVATAR}
+export CYBER_DOJO_AVATAR_NAME=${AVATAR_NAME}
 export CYBER_DOJO_RUNNER=processful
-export CYBER_DOJO_SANDBOX=/sandboxes/${AVATAR}
+export CYBER_DOJO_SANDBOX=/sandboxes/${AVATAR_NAME}
 
 cd ${CYBER_DOJO_SANDBOX}
 
 grep -q -c Alpine /etc/issue >/dev/null 2>&1
 if [ $? -eq 0 ]; then
   # On Alpine's ps, the avatar-user's name is truncated to 8 chars
-  readonly PS_AVATAR=`echo ${AVATAR} | cut -c -8`
+  readonly PS_AVATAR=`echo ${AVATAR_NAME} | cut -c -8`
 fi
 
 grep -q -c Ubuntu /etc/issue >/dev/null 2>&1
 if [ $? -eq 0 ]; then
-  readonly PS_AVATAR=${AVATAR}
+  readonly PS_AVATAR=${AVATAR_NAME}
 fi
 
-su ${AVATAR} -c "timeout -s KILL ${MAX_SECONDS}s sh ./cyber-dojo.sh"
+su ${AVATAR_NAME} -c "timeout -s KILL ${MAX_SECONDS}s sh ./cyber-dojo.sh"
 status=$?
 ps -o user,pid | grep "^${PS_AVATAR}\s" | awk '{print $2}' | xargs -r kill -9 >/dev/null 2>&1
 exit ${status}
